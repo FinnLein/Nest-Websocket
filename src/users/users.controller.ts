891572bda9930663057ae025456cfca5cd2bd2ac
FinnLein <ekimenko.nikita@list.ru>
2026-01-05 19:37:20 +0300

    feat users: refactor users route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get, Param } from '@nestjs/common'
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common'
 import { UsersService } from './users.service'
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) { }
 
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async getAll() {
+    return this.getAll()
+  }
+
   @Get('by-name/:name')
+  @HttpCode(HttpStatus.OK)
   async getByName(@Param('name') name: string) {
     return this.usersService.getByName(name)
   }
   @Get('by-email/:email')
+  @HttpCode(HttpStatus.OK)
   async getByEmail(@Param('email') email: string) {
     return this.usersService.getByEmail(email)
   }
